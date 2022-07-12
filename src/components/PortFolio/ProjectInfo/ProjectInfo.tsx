28276416347a737dfc13bd769d9b0e4be5fa22e5
oajh04 <cloudtubekr@naver.com>
2022-07-12 23:16:51 +0900

    ⚡️ :: 포트폴리오 상세보기 화면

---
+++ src/components/PortFolio/ProjectInfo/ProjectInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ProjectInfo = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>This is profile</Text>
+    </View>
+  );
+};
+
+export default ProjectInfo;
