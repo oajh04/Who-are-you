627fb7b8cf8dd654f5a6d22742c2c69a6b8e3c08
oajh04 <cloudtubekr@naver.com>
2022-07-13 23:00:37 +0900

    ⚡️ :: Default Box

---
+++ src/components/common/DefaultBox/DefaultBox.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+interface Props {
+  children: React.ReactNode;
+  name: string;
+}
+
+const DefaultBox = ({children, name}: Props) => {
+  return (
+    <View style={styles.wrapper}>
+      <Text style={{fontSize: 20, marginBottom: 10, color: 'black'}}>
+        {name}
+      </Text>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 10,
+    backgroundColor: 'white',
+    margin: 5,
+    marginLeft: 0,
+    marginRight: 0,
+  },
+});
+
+export default DefaultBox;
