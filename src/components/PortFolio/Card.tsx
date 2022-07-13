6a0988219e7e22ebbebb2a3e76005c3edf2a3bf0
oajh04 <cloudtubekr@naver.com>
2022-07-13 23:00:51 +0900

    ⚡️ :: Card

--- src/components/PortFolio/Card.tsx
+++ src/components/PortFolio/Card.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text} from 'react-native';
+import DefaultBox from '../common/DefaultBox/DefaultBox';
 
 const Card = () => {
   return (
-    <View style={styles.wrapper}>
-      <Text>Pick</Text>
+    <DefaultBox name="Pick">
       <Text>교내 방과후 출석부 시스템입니다.</Text>
       <Text>21.07 ~ 22.07</Text>
-    </View>
+    </DefaultBox>
   );
 };
-
-const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: 'white',
-    margin: 5,
-    marginLeft: 0,
-    marginRight: 0,
-    padding: 15,
-  },
-});
 
 export default Card;
