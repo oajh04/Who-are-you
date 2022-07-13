669c7b3b8659a8595b032fd4615bf12034a0bcaf
oajh04 <cloudtubekr@naver.com>
2022-07-13 23:00:44 +0900

    ⚡️ :: Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image, ScrollView} from 'react-native';
+import DefaultBox from '../common/DefaultBox/DefaultBox';
 
 const Home = () => {
   return (
     <>
       <View style={styles.wrapper}>
-        <Text style={styles.title}>한준호</Text>
+        <View style={styles.profile}>
+          <Text style={styles.title}>한준호</Text>
+          <Image
+            source={{uri: 'https://picsum.photos/id/1048/5616/3744'}}
+            style={styles.image}
+          />
+        </View>
+        <ScrollView style={styles.contentWrapper}>
+          <DefaultBox name={'학력'}>
+            <Text>대덕소프트웨어마이스터고등학교</Text>
+            <Text>20.03 ~ 재학중</Text>
+          </DefaultBox>
+          <DefaultBox name={'기술 스택'}>
+            <Text>정보처리기능사</Text>
+          </DefaultBox>
+          <DefaultBox name={'자격증'}>
+            <Text>정보처리기능사</Text>
+          </DefaultBox>
+          <DefaultBox name={'수상 및 기타 이력'}>
+            <Text>나Be 한마당 코딩경진대회 동상</Text>
+          </DefaultBox>
+          <DefaultBox name={'포트폴리오'}>
+            <Text>https://oajh04.super.site</Text>
+          </DefaultBox>
+        </ScrollView>
       </View>
     </>
   );
@@ -13,18 +38,30 @@ const Home = () => {
 
 const styles = StyleSheet.create({
   wrapper: {
-    padding: 10,
+    backgroundColor: '#121212',
+  },
+  contentWrapper: {
+    backgroundColor: '#efefef',
+    borderTopStartRadius: 10,
+    borderTopEndRadius: 10,
+    paddingTop: 5,
+  },
+  profile: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
   },
   title: {
-    paddingVertical: 8,
-    borderWidth: 1,
-    borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: '#61dafb',
-    color: '#20232a',
-    textAlign: 'center',
-    fontSize: 30,
+    fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
   },
 });
 
