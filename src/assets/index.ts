fe16e9444d287894e803a28f5d4b0fc4c092e87a
oajh04 <cloudtubekr@naver.com>
2022-07-14 16:13:29 +0900

    ⚡️ :: svg 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export {default as HomeSVG} from './svg/HomeSVG.svg';
+export {default as PortfolioSVG} from './svg/PortfolioSVG.svg';
+export {default as GithubSVG} from './svg/GithubSVG.svg';
+export {default as PhoneSVG} from './svg/PhoneSVG.svg';
+export {default as EmailSVG} from './svg/EmailSVG.svg';
