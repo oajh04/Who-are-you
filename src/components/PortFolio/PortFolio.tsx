00f6b5a2c7afeaeb7333969c9117d70f872611ec
oajh04 <cloudtubekr@naver.com>
2022-07-14 16:14:33 +0900

    ⚡️ :: PortFolio 컴포넌트

--- src/components/PortFolio/PortFolio.tsx
+++ src/components/PortFolio/PortFolio.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, ScrollView, TouchableOpacity} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 import Card from './Card';
 import Category from './Category';
 
 const PortFolio = ({navigation}: any) => {
+  const [data, setData] = useState([{}]);
+  const projectCollection = firestore().collection('projectList');
+
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const res = await projectCollection.get();
+        setData(res.docs?.map((doc: any) => ({...doc.data(), id: doc.id})));
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    };
+
+    callApi();
+  });
+
   return (
     <View>
       <Category />
       <ScrollView>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('ProjectInfo', {id: 1})}>
-          <Card />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-          <Card />
-        </TouchableOpacity>
+        {data.map((i: any) => {
+          return (
+            <TouchableOpacity
+              key={i.id}
+              onPress={() => navigation.navigate('ProjectInfo', {id: i.id})}>
+              <Card {...i} />
+            </TouchableOpacity>
+          );
+        })}
       </ScrollView>
     </View>
   );
