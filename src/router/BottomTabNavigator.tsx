390a3b9a063424eb1a442a0322e2c89b07b3a3b8
oajh04 <cloudtubekr@naver.com>
2022-07-15 22:30:17 +0900

    ⚡️ :: BottomTabNavigator

--- src/router/BottomTabNavigator.tsx
+++ src/router/BottomTabNavigator.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Home from '../components/Home/Home';
+import Profile from '../components/Profile/Profile';
 import PortFolio from '../components/PortFolio/PortFolio';
-import CreateProject from '../components/CreateProject/CreateProject';
-import {HomeSVG, PortfolioSVG} from '../assets';
+import {HomeSVG, LogOutSVG, PortfolioSVG, ProfileSVG} from '../assets';
+import Home from '../components/Home/Home';
+import {Text, View} from 'react-native';
+import {setUId} from '../libs/functions/idManagement';
 
-const Tab = createBottomTabNavigator();
+export type BottomTabParamList = {
+  Home: undefined;
+  PortFolio: undefined;
+  Profile: undefined;
+};
 
-const BottomTabNavigator = () => {
+const Tab = createBottomTabNavigator<BottomTabParamList>();
+
+const BottomTabNavigator = ({navigation}: any) => {
+  const onLogOut = () => {
+    setUId('');
+    navigation.navigate('Login');
+  };
+
   return (
     <>
       <Tab.Navigator
@@ -18,14 +31,6 @@ const BottomTabNavigator = () => {
           name="Home"
           component={Home}
           options={{
-            title: '프로필',
-            headerStyle: {
-              backgroundColor: '#121212',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
             tabBarIcon: () => {
               return <HomeSVG />;
             },
@@ -40,7 +45,28 @@ const BottomTabNavigator = () => {
             },
           }}
         />
-        <Tab.Screen name="CreateProject" component={CreateProject} />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            title: '프로필',
+            headerStyle: {
+              backgroundColor: '#121212',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            tabBarIcon: () => {
+              return <ProfileSVG />;
+            },
+            headerRight: () => (
+              <Text style={{marginRight: 15}} onPress={onLogOut}>
+                <LogOutSVG />
+              </Text>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </>
   );
