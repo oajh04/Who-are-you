9d1de71911dd3ddb5ed97d024cda1521fe0e50d8
oajh04 <cloudtubekr@naver.com>
2022-07-16 02:14:33 +0900

    ⚡️ :: App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Navigation from './src/router';
+import {ToastProvider} from 'react-native-toast-notifications';
 
 const App = () => {
+
   return (
-    <>
+    <ToastProvider placement="bottom">
       <Navigation />
-    </>
+    </ToastProvider>
   );
 };
 
