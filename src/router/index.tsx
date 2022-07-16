ba2ef744a1e8053cc23e982f111327e462241f3d
oajh04 <cloudtubekr@naver.com>
2022-07-16 23:06:05 +0900

    ⚡️ :: safearea 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootNavigator from './RootNavigation';
+import {SafeAreaView} from 'react-native';
 
 const Navigation = () => {
   return (
     <>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <SafeAreaView style={{flex: 1}}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </SafeAreaView>
     </>
   );
 };
