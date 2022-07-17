e553b95d9fc738bc45e3f03c9af99045c17a94b9
oajh04 <cloudtubekr@naver.com>
2022-07-17 22:35:41 +0900

    ⚡️ :: My PortFolio 데이터 받아오기

--- src/components/PortFolio/PortFolio.tsx
+++ src/components/PortFolio/PortFolio.tsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {View, ScrollView, TouchableOpacity} from 'react-native';
 import Card from './Card';
-import Category from './Category';
 import {RootStackParamList} from '../../router/RootNavigation';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {getProjectList} from '../../libs/apis/project';
+import {getUId} from '../../libs/functions/idManagement';
 
 interface Props {
   navigation: StackNavigationProp<RootStackParamList, 'Root'>;
@@ -14,21 +14,24 @@ const PortFolio = ({navigation}: Props) => {
   const [data, setData] = useState<any>([]);
 
   useEffect(() => {
-    getProjectList().then(res => {
-      setData(res.docs?.map((doc: any) => ({...doc.data(), id: doc.id})));
+    getUId().then(response => {
+      if (response) {
+        getProjectList(response).then(res => {
+          setData(res.docs?.map((doc: any) => ({...doc.data(), id: doc.id})));
+        });
+      }
     });
   }, []);
 
   return (
     <View>
-      <Category />
       <ScrollView>
         {data.map((i: any) => {
           return (
             <TouchableOpacity
               key={i.id}
               onPress={() => navigation.navigate('ProjectInfo', {id: i.id})}>
-              <Card {...i} key={`${i.id}-card`} />
+              <Card {...i} />
             </TouchableOpacity>
           );
         })}
